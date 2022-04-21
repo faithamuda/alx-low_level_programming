9bfda066b2dbb1a6877c4c216840b95ccd9e49b3
nifemifaith07 <nifemifaith07@gmail.com>
2022-04-20 18:49:13 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include  <stdio.h>
+
+/**
+ * main - Entry  point 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int digits;
+	int alpha;
+
+	for (digits = 0; digits <= 9; digits++)
+	{
+		putchar('0' + digits);
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
