4adb1a598e7c1a04bd00828c00bead13cd8678d7
nifemifaith07 <nifemifaith07@gmail.com>
2022-04-22 02:53:21 -0700

    print every possible combo of single digit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,12 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 	{
 		putchar('0' + i);
-		putchar (',');
-		putchar(' ');
+		
+		if (i != 9)
+		{
+			putchar (',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
