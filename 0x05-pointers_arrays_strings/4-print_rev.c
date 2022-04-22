413de42d7383762cd0da52a60683bfe75c7d8d96
nifemifaith07 <nifemifaith07@gmail.com>
2022-04-21 23:02:46 -0700

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - prints in reverse
+ * @s: input string
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int i, j, temp;
+	i = 0;
+
+	while (s[i] != '\O')
+	{
+		i++;
+	}
+
+	temp = i;
+
+	for (j = temp - i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
