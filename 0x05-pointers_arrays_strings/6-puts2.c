6d8743a02c3d8e6a0138faf0f4c4841f5d3dce2b
nifemifaith07 <nifemifaith07@gmail.com>
2022-04-23 07:40:56 -0700

    print every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,11 +13,11 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
 		{
-			_putchar(s[i]);
+			_putchar(str[i]);
 		}
 		else
 		continue;
