92720d8c4ec8a127a26c0ada834d203a10a49ccd
nifemifaith07 <nifemifaith07@gmail.com>
2022-04-22 23:32:54 -0700

    print all possible combo of 2-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
 /**
-* main - Entry point
-* program that prints all possible combinations
-* of two two-digit numbers.
-* The numbers should range from 0 to 99
-* The two numbers should be separated by a space
-* All numbers should be printed with two digits
-* 1 should be printed as 01
-* The combination of numbers must be separated 
-* by comma, followed by a space
-* The combinations of numbers should be printed 
-* in ascending order
-
-* Return: Always 0 (Success)
-/*
+ * main - Entry point
+ * program that prints all possible combinations
+ * of two two-digit numbers.
+ * The numbers should range from 0 to 99
+ * he two numbers should be separated by a space
+ * All numbers should be printed with two digits
+ * The combination of numbers must be separated 
+ * by comma, followed by a space
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -42,4 +39,5 @@ int main(void)
 	}
 	putchar('\n');
 
-	return (0);				
+	return (0);
+}
