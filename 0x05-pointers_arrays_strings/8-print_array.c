446ac2fec7fbe3109351ac370d0f29ee6d0e3d34
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-23 22:28:36 +0100

    Prints number of elements in  an array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print_array - prints n element of an array of integers 
+ * print_array - prints n element of an array of integers
  *@a: pointer to the array
  *@n: number of elements in the array
  *
@@ -12,6 +12,7 @@
 void print_array(int *a, int n)
 {
 	int i;
+
 	for (i = 0; i < n; i++)
 	{
 		if (i == (n - 1))
