ff7751b4632fb86d5f23ee70734a25fecad9df79
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-23 17:15:48 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -37,7 +37,8 @@ void puts_half(char *str)
 		 * if lenght is odd
  		 * j is half of length - 1
  		*/
-		for (j = (len - 1) / 2; j < len; str[j] != '\0'; j++)
+		for (j = (len - 1) / 2; str[j] != '\0'; j++)
+			j < len;
 			_putchar(str[1 + j]);
 	}
 	_putchar('\n');
