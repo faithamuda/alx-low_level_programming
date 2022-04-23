0d2d9b37e61a78f0ad757e224f1b5358d56b00e1
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-23 16:27:47 +0100

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * puts_half - prints second half of string
+ * @str: pointer to string
+ * if numbers of characters is odd,
+ * print the last n characters
+ * where n = (length_of_the_string - 1) / 2
+ *
+ * Return: Nothing 
+*/
+
+void puts_half(char *str)
+{
+	int i, j, k, l;
+
+	/* find length pf array to null character */
+	while (i >= 0)
+	{
+		if (str[i] == '\0')
+			break;
+		i++;
+	}
+	/* check if number of character is even or odd */
+	if (i % 2 == 0)
+	{
+		/** 
+ 		 * if lenght is even
+		 * j is half of total lenght
+ 		*/
+		j = i / 2;
+		for (k = j; k < i; k++)
+			_putchar(str[k]);
+	}
+	else
+	{
+		/**
+		 * if lenght is odd
+ 		 * j is half of length - 1
+ 		*/
+		j = (i - 1) / 2;
+		for (l = j; l < i; l++)
+			_putchar(str[l]);
+	}
+	_putchar('\n');
+}
