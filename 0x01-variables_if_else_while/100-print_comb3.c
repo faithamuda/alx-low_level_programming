32fe22eb1b989e38eed18006cd7cf80c45e759db
nifemifaith07 <nifemifaith07@gmail.com>
2022-04-22 22:03:20 -0700

    100-print_comb3.c updated

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,25 +13,24 @@ int main(void)
 
 	for (i = 0; i <= 8; i++)
 	{
-                for (j = 1; j <= 9; j++)
-		{       
-                         if (j > i)
-                         {
-			        putchar('0' + i);
-                                putchar('0' + j);
+		for (j = 1; j <= 9; j++)
+		{
+			if (j > i)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
 
-		                 if (i != 8 || j != 9)
-		                 {
-			                 putchar(',');
-			                 putchar(' ');
-		                 }
-                          }
-                          else
-                          continue;
-                   }
-          }
-	  putchar('\n');
+				if (i != 8 || j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			else
+			continue;
+		}
+	}
+	putchar('\n');
 
-	  return (0);
+	return (0);
 }
-
