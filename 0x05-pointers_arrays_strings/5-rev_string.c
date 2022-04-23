e1285761853850cad26156ead51437910d86479d
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-23 09:18:01 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,10 +13,10 @@ void rev_string(char *s)
 	char ch;
 
 	/*find string length without null char*/
-	for (l = 0; s[l] != '\0'; ++l)
+	for (l = 0; s[l] != '\0'; l++)
 		;
 	/*swap the string by looping to half the string*/
-	for (i = 0; i < l / 2; ++i)
+	for (i = 0; i < l / 2; i++)
 	{
 		ch = s[i];
 		s[i] = s[l - i - 2]; /*-1 because the array starts from 0*/
