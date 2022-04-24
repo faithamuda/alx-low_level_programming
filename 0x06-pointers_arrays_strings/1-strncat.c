03a3d04f3ac74ea0f9e632b1f2f87ee0189f5aa0
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-24 18:38:09 +0100

    Create 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include  "main.h"
+
+/**
+ * _strncat - function that appends n bytes of strings
+ * from *src to *dest
+ * @dest: destination for appended n strings
+ * @src: source of strings to be appended
+ * @n: max number of characters to be appended
+ *
+ * Return: pointer to string dest
+*/
+ 
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	while (dest[i] != '\0')
+		i++;
+	j = 0;
+	while (src[j] != '\0'; j <= n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
