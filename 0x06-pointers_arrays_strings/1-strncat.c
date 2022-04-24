b4aea7f414091569c8372c8fecefb4e6d22ef0c8
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-24 18:40:38 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,7 +18,7 @@ char *_strncat(char *dest, char *src, int n)
 	while (dest[i] != '\0')
 		i++;
 	j = 0;
-	while (src[j] != '\0'; j <= n)
+	while (src[j] != '\0' && j <= n)
 	{
 		dest[i] = src[j];
 		i++;
