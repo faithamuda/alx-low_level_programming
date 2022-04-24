a3d15d806c574ec6bab6de44211487772fe3d6e7
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-24 10:44:25 +0100

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,4 +5,19 @@
  * @src: string source
  * @dest: string destination
  *
- *
+ * Return: pointer to the resulting string dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+i = j = 0;
+while (dest[i] != '\0')
+i++;
+while (((dest[i]) = (src[j])))
+{
+i++;
+j++:
+}
+return (dest);
+}
