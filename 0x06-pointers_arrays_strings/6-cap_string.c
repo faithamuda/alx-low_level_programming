1f9d535123e097c3f66267c277a6148647de750f
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-25 17:06:58 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,4 +2,34 @@
 
 /**
  * cap_string - function that capitalizes all words in a string
- * @c: 
+ * @src: pointer to string
+ * 
+ * Return: value of src
+*/
+
+char *cap_string(char *src)
+{
+	int count;
+
+	count = 0;
+	while (src[count] != '\0')
+	{/* if next character after count is a char , capitalise it */
+		if (src[0] >= 97 && src[0] <= 122)
+		{
+			src[0] = src[0] - 32;
+		}
+		if (src[count] == ' ' || src[count] == '\t' || src[count] == '\n'
+		    || src[count] == ',' || src[count] == ';' || src[count] == '.'
+		    || src[count] == '.' || src[count] == '!' || src[count] == '?'
+		    || src[count] == '"' || src[count] == '(' || src[count] == ')'
+		    || src[count] == '{' || src[count] == '}')
+		{
+			if (src[count + 1] >= 97 && src[count + 1] <= 122)
+			{
+				src[count + 1] = src[count + 1] - 32;
+			}
+		}
+		count++;
+	}
+	return (src);
+}
