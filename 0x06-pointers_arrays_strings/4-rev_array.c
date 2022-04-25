516ebb03c9fda0c6158e6c3721fc5bb00a11896e
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-25 06:03:54 +0100

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,31 @@
+#include  "main.h"
+
+/**
+ * reverse_array - function that reverses the content of an array of integers.
+ * @a: pointer to array
+ * @n: no of elements in the array
+ *
+ * Return: Nothing
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i, temp, h;
+
+	i = 0;
+	h = n - 1;
+	/**
+	 * set value in array a in temp
+ 	* then place the last array in
+	* the first array then place
+ 	* value in temp to last array
+	*/
+	while i > h
+	[
+		temp = a[i];
+		a[i] = a[h];
+		a[h] = temp;
+		i++;
+		h++;
+	}
+}
