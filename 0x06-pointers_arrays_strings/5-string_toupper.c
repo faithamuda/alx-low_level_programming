77734ac848ded4e0418d684127c5188914fec4d3
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-25 06:33:48 +0100

    Create 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - Function that changes lowercase to uppercase
+ * @s: pointer toinput char
+ *
+ * Return: s
+*/
+
+char *string_toupper(char *s)
+{
+	int j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		if (j > 96 && j < 123)
+			s[j] = j - 32;
+	}
+	return (s);
+}
