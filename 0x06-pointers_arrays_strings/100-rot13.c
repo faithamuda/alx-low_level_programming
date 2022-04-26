3cbf17a54a0538e77690525e43735f02e7fa45c5
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-26 02:00:15 +0100

    Create 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * rot13 - function that encodes a string into using rot13. 
+ * @str: pointer to string to be encoded.
+ *
+ * You can only use if statement once and only use two loops in your code. 
+ * You are not allowed to use switch.
+ *
+ * Return: the encoded string.
+*/
+
+char *rot13(char *str)
+{
+	int x = 0, y;
+	char alpha[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	while (str[x] != '\0')
+	{
+		for (y = 0; y <= 52; y++)
+		{
+			if (str[x] == alpha[y])
+			{				
+				str[x] = rot13[y];			
+				break;			
+			}
+		}
+		x++;	
+	}
+	return (str);
+}
