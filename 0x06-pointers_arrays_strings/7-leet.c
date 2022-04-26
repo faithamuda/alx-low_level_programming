8763c803b63e34100666c71aa26be64a002cb96a
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-26 01:08:12 +0100

    3nc0d3s 4 s7ring in70 1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -16,7 +16,7 @@ char *leet(char *str)
 {
 	int y, x = 0;
 	char subs[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-	char src[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'T',};
+	char src[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L',};
 
 	/* check through str starting from first character */
 	while (str[x])
