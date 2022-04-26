93e6664e9d6b83dc4b0ebb1dbb9b15b37b2d988d
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-26 03:09:20 +0100

    Print integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -12,14 +12,16 @@ void print_number(int n)
 	unsigned int j;
 
 	j = n;
+	/* check if number is -ve */
 	if (n < 0)
 	{
 		_putchar(45);
 		j = -n;
 	}
+	/* print digits one by one using recursion */
 	if (j / 10)
-	{
-		print_number(j / 10);
+	{/* recursive call🤷‍♀️ */
+		print_number(j / 10); 
 	}
 	_putchar('0' + (j % 10));
 }
