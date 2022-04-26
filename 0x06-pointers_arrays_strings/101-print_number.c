4ae9cc9f9f76f5e13e66da626474686c5eec51bd
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-26 02:43:36 +0100

    Create 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include  "main.h"
+
+/**
+ * print_number - prints an integers
+ * @n: integer to print
+ *
+ * Return: void (Success)
+*/
+
+void print_number(int n)
+{
+	int j;
+
+	if (n < 0)
+	{
+		j = -n;
+		_putchar(45);
+	]
+	else
+		j = n;
+		
+	if (b / 10)
+	{
+		_putchar('0' + (b / 10));
+		_putchar( '0' + (b % 10));
+	}
+]
