5465904a14140a5efb140f34ff83e122826721ac
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-26 03:05:03 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -11,12 +11,15 @@ void print_number(int n)
 {
 	unsigned int j;
 
+	j = n;
 	if (n < 0)
 	{
 		_putchar(45);
 		j = -n;
 	}
 	if (j / 10)
+	{
 		print_number(j / 10);
+	}
 	_putchar('0' + (j % 10));
 }
