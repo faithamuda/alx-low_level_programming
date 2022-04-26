a97b898893f6de2802085e783f9736d03d7856f7
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-26 01:06:17 +0100

    3nc0d3s 4 s1ing in10 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - function that encodes a string into 1337
+ * Letters a & A replaced by 4
+ * Letters e & E replaced by 3
+ * Letters o & O replaced by 0
+ * Letters t & T replaced by 7
+ * Letters l & L replaced by 1
+ * str: pointer to string
+ *
+ * Return: encoded string
+*/
+
+char *leet(char *str)
+{
+	int y, x = 0;
+	char subs[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char src[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'T',};
+
+	/* check through str starting from first character */
+	while (str[x])
+	{/* count through src and subs using y */
+		for (y = 0; y < 10; y++)
+		{/* check if str char corresponds to src char and substitute */
+			if (str[x] == src[y])
+				str[x] = subs[y];
+		}/* move to next str char */
+		x++;
+	}
+	return (str);
+}
