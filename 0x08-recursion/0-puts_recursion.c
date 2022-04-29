81f0e99bdd70ab976c6194423ee611e0fd09ebf9
nifemifaith07 <nifemifaith07@gmail.com>
2022-04-29 02:17:41 -0700

    added

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,15 +10,13 @@
 
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (*s[i] == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		_putchar(*s[i]);
-		_puts_recursion(*s[i + 1]);
+		_putchar(*s);
+		_puts_recursion(*s + 1);
 	}
 }
