dfb55ce49998e3f25200e4653718d76cc0a63d47
nifemifaith07 <nifemifaith07@gmail.com>
2022-04-29 04:38:06 -0700

    print in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,11 +7,11 @@
  * Return: nothing
  */
 
-void _print_rev_recursion(char *s);
+void _print_rev_recursion(char *s)
 {
-if (*s != '\0')
-{
-_print_rev_recursion(s + 1)
-_putchar(*s)
-}
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s)
+	}
 }
