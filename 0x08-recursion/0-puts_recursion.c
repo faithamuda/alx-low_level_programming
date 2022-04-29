3610bcc0ba4cee3d768c7f31a5532b96746bcd8b
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-29 09:36:09 +0100

    Create

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _puts_recursion - function that prints a string  
+* Followed by new line
+* @s: pointer to string
+*
+* Return: s
+*/
+
+void _puts_recursion(char *s)
+{
+int i = 0
+if (s[i] == '\0')
+_putchar('\n');
+else
+_putchar(s[i]);
+_puts_recursion(s[i + 1]);
+return (s);
+}
