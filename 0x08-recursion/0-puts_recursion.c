cb0e72fa6c1f199ba2acc3addf0d8bd1af4e4509
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-29 10:11:02 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,13 +12,13 @@ void _puts_recursion(char *s)
 {
 	int i = 0;
 
-	if (s[i] == '\0')
+	if (*s[i] == '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		_putchar(s[i]);
-		_puts_recursion(s[i + 1]);
+		_putchar(*s[i]);
+		_puts_recursion(*s[i + 1]);
 	}
 }
