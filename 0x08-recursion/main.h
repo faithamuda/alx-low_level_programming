eea293705351749a6d7a0a01c6ecfdafa8acb168
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-04-29 10:35:28 +0100

    Update main.h

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,6 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
 int _strlen_recursion(char *s);
 void _print_rev_recursion(char *s);
 void _puts_recursion(char *s);
@@ -7,3 +10,6 @@ int factorial(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+int _putchar(char);
+
+#endif
