4c2f581a5415e84a6b59edc7bfdc34d1d1b838a9
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-05-01 01:44:04 +0100

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry point 
+ * @void: no argument
+ *
+ * Return: Always 0 (Successs)
+ */
+
+int main(void)
+{
+	int j = 0;
+	char nife[] = "_putchar";
+
+	while(j < 8)
+	{
+		_putchar(nife[j]);
+		j++;
+	}
+	_putchar('\n');
+	return (0);
+}
