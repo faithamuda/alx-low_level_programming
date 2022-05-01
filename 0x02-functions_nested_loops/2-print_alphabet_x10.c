3d20abde7be3c210449a3f99d55b9f5abd05b4dc
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-05-01 02:20:09 +0100

    Prints 10*alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10*alphabets in lowercase
+ * followed by newline
+ * @void: no argument
+ *
+ * Return: Nothing
+ */
+ 
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int j = 0;
+
+	while (j < 10)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+	}
+		j++;
+	_putchar('\n');
+}
