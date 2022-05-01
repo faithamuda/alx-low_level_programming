1e6060ae2cb8334696a498adf528db39e77b8570
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-05-01 01:33:15 +0100

    Create main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define MAIN_H_
+
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+int add(int, int);
+void times_table(void);
+void jack_bauer(void);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
