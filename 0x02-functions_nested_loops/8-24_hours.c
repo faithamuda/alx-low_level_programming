a85da9224b6ac8ac68a8108771fb9b0ae876605b
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-05-01 08:18:57 +0100

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+Write a function that , starting from 00:00 to 23:59.starting from 00:00 to 23:59.
+
+Prototype: void (void);
+You can listen to this soundtrack while coding :)
+  
+#include "main.h"
+  
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
+ * @void: no argument 
+ *
+ * Return: Nothing
+ */
+ 
+void jack_bauer(void)
+{
+int j, k;
+for (j = 0; j < 24; j++)
+{
+for (k = 0; k < 60; k++)
+{
+_putchar
+_putchar
+_putchar
+_putchar
+_putchar
+}
+}
+
+}
