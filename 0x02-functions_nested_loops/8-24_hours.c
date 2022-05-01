613ee70b5f465a5348db58279f1c9879566311af
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-05-01 09:40:42 +0100

    Print minutes in a day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,13 +1,13 @@
 #include "main.h"
-  
+
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
- * @void: no argument 
+ * @void: no argument
  *
  * Return: Nothing
  */
- 
+
 void jack_bauer(void)
 {
 	int j, k;
