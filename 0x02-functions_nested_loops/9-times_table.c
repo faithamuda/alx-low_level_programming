5823a4b9bf47ab3f86ebf3b2dd43374ca95ca2a7
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-05-01 09:56:35 +0100

    Create 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+
+I 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+$ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9$
+
+j is o
+for j is less equal 9
+for k is 0
+k is less equal 9
+  print k*j
+    if k is not equal 9
+    print, nd space
+  print newline
+  
+  
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0
+ * @void: no argument
+ *
+ * Return: Nothing
+ */
+ 
+void times_table(int n)
+{
+int j = 0, k;
+for ( 
+}
+	
+	
