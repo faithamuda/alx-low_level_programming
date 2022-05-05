6a00f7ab6419c2281d0f3fc519c8ae5256e1756c
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-05-05 07:18:39 +0100

    Update 100-atoi.c

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,16 +1,28 @@
 #include "main.h"
 
 /**
- * atoi - function that converts a string
- *       into an integer
- *
- * @s: string input pointer
- *
- * Return: 0 if there is no number
-*/
+ * _atoi - convert a string to an integer.
+ * @s: char type string
+ * Return: integer converted
+ */
 
 int _atoi(char *s)
 {
-if (s >= 48)
-return (0);
+	int sign = 1, resp = 0, firstNum;
+
+	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
+	{
+		if (s[firstNum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (int i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		resp *= 10;
+		resp += (s[i] - 48);
+	}
+
+	return (sign * resp);
 }
