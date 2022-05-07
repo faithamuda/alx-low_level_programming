fce0bfc6dd0786b077a65be26ffabe20d5843973
nifemifaith07 <nifemifaith07@gmail.com>
2022-05-07 16:13:58 -0700

    prints all arguments

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,9 +12,10 @@
 
 int main(int argc, char *argv[])
 {
-int count;
+	int count;
 
-for (count = 0; count < argc; count++)
-printf("%s\n", argv[count]);
-return (0);
+	for (count = 0; count < argc; count++)
+	{
+		printf("%s\n", argv[count]);
+		return (0);
 }
