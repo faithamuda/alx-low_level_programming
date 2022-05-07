421a191fa42a9ae32a615a89bbb8bf0b261e06a2
nifemifaith07 <nifemifaith07@gmail.com>
2022-05-07 16:29:39 -0700

    added

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,12 +14,16 @@
 
 int main(int argc, char *argv[])
 {
-int mul = atoi(argv[1]) * atoi(argv[2]);
+	int mul = atoi(argv[1]) * atoi(argv[2]);
 
-if (argc = 3)
-printf("%d\n", mul);
-return (0);
-else
-printf("Error\n");
-return (1);
+	if (argc == 3)
+	{
+		printf("%d\n", mul);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
 }
