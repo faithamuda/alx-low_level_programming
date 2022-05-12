ffdc4382d53d94492def2c6e061e80e4b093b110
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-05-12 23:56:04 +0100

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,3 @@
-
-The  () function returns a pointer to a new string which is a
-duplicate of the string str. Memory for the new string is obtained with malloc, and can be freed with free.
-Returns NULL if str = NULL
-On success, the _strdup function returns 
-a pointer to the duplicated string. It returns NULL if insufficient memory was available
-
 #include "main.h"
 #include <stdlib.h>
 
