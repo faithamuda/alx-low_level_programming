545b02426852f4e2f1a2afcc210fbef9b6a515b9
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-05-16 13:31:19 +0100

    Update main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,5 +3,10 @@
 
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
+void free_grid(int **grid, int height);
+int **alloc_grid(int width, int height);
+char *str_concat(char *s1, char *s2);
 
 #endif
