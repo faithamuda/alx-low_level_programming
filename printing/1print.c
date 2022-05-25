144c71e5e16782ee031576248518671558279f65
nifemifaith07 <77938126+nifemifaith07@users.noreply.github.com>
2022-05-25 11:06:11 +0100

    Create 1print.c

---
+++ printing/1print.c
@@ -0,0 +1,53 @@
+#include "main. h"
+ 
+/**
+ * _printf - prints according to format
+ * @format: formated string
+ * Return: no of char printed
+ */
+
+int _printf(const char *format, ...)
+{
+char *str;
+char *lenght
+int i, j,count;
+count = 0;
+va_list arg;
+va_start(arg, format);
+
+if (!format || format[0] = % && !format[1])
+return (-1);
+if (format[0] = % && format[1] = ' ' && !format[2])
+return (-1);
+
+ for (length = format *lenght != '\0' lenght++)
+{
+i = 0
+while (lenght[i] != %)
+{
+putchar(lenght[i])
+i++
+count ++
+}
+i++
+
+if (lenght[i] = '%')
+{
+putchar(lenght[i]);
+count++
+}
+else if (lenght[i] = 'c')
+ch = va_arg(arg, int);
+putchar(ch)
+count++
+}
+else if (length[i] = 's')
+str = va_arg(arg, char *)
+j = 0
+while str != NULL 
+putchar(str[j])
+j++
+count++
+}
+va_end(arg)
+return (count)
